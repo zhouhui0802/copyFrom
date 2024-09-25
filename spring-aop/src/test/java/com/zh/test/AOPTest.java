e6ac924fe0527eafa16fa8f7b5ebc50c4fd57ab1
zhouhui0802 <1741484908@qq.com>
2024-09-25 20:36:44 +0800

    xml的切面分析

--- spring-aop/src/test/java/com/zh/test/AOPTest.java
+++ spring-aop/src/test/java/com/zh/test/AOPTest.java
@@ -1,6 +1,6 @@
 package com.zh.test;
 
-import com.zh.spring.Calculator;
+import com.zh.xml.Calculator;
 import org.junit.Test;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
@@ -10,6 +10,15 @@ public class AOPTest {
     @Test
     public void testAOPByAnnotation(){
         ApplicationContext ioc=new ClassPathXmlApplicationContext("aop-annotation.xml");
+
+        Calculator calculator=ioc.getBean(Calculator.class);
+
+        calculator.add(1,2);
+    }
+
+    @Test
+    public void test1(){
+        ApplicationContext ioc=new ClassPathXmlApplicationContext("aop-xml.xml");
 
         Calculator calculator=ioc.getBean(Calculator.class);
 
