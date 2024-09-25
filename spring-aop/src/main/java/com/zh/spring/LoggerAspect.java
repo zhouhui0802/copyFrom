129e6513223189b8380c962c9cf9428eab79cb0c
zhouhui0802 <1741484908@qq.com>
2024-09-25 11:47:07 +0800

    环绕切面

--- spring-aop/src/main/java/com/zh/spring/LoggerAspect.java
+++ spring-aop/src/main/java/com/zh/spring/LoggerAspect.java
@@ -1,10 +1,9 @@
 package com.zh.spring;
 
 import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.Signature;
-import org.aspectj.lang.annotation.Aspect;
-import org.aspectj.lang.annotation.Before;
-import org.aspectj.lang.annotation.Pointcut;
+import org.aspectj.lang.annotation.*;
 import org.springframework.stereotype.Component;
 
 import java.util.Arrays;
@@ -35,5 +34,45 @@ public class LoggerAspect {
         Signature signature=joinPoint.getSignature();
         Object args[]=joinPoint.getArgs();
         System.out.println("LoggerAspect new 方法："+signature.getName()+"new 参数："+ Arrays.toString(args));
+    }
+
+    @After("pointCut()")
+    public void afterAdviceMethod(JoinPoint joinPoint){
+        Signature signature=joinPoint.getSignature();
+
+        System.out.println("loggerAspect, 后置通知 方法："+signature.getName()+",执行完毕");
+    }
+
+    @AfterReturning(value = "pointCut()", returning = "result")
+    public void afterReturningAdviceMethod(JoinPoint joinPoint,Object result){
+        Signature signature=joinPoint.getSignature();
+
+        System.out.println("loggerAspect, 返回通知 方法："+signature.getName()+",执行完毕 结果："+result);
+    }
+
+    @AfterThrowing(value = "pointCut()", throwing = "ex")
+    public void afterThrowingAdviceMethod(JoinPoint joinPoint,Throwable ex){
+        Signature signature=joinPoint.getSignature();
+
+        System.out.println("loggerAspect, 返回通知 方法："+signature.getName()+",执行完毕 异常： "+ex);
+    }
+
+    @Around(value = "pointCut()")
+    public Object aroundAdviceMethod(ProceedingJoinPoint joinPoint){
+        Object result=null;
+        try{
+            System.out.println("环绕通知-》前置通知");
+
+            result=joinPoint.proceed();
+
+            System.out.println("环绕通知-》返回通知");
+        }catch(Throwable throwable){
+            throwable.printStackTrace();
+            System.out.println("环绕通知-》异常通知");
+        }finally {
+            System.out.println("环绕通知-》后置通知");
+        }
+
+        return result;
     }
 }
