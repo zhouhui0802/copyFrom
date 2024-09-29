54665d5561ace490f56965ce550a9b9a7d7a6bc7
zhouhui0802 <1741484908@qq.com>
2024-09-29 10:54:24 +0800

    各种风格的注解

--- springmvc_demo/src/main/java/com/zh/controller/TestRequestMappingTest.java
+++ springmvc_demo/src/main/java/com/zh/controller/TestRequestMappingTest.java
@@ -1,6 +1,7 @@
 package com.zh.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
@@ -18,6 +19,17 @@ public class TestRequestMappingTest {
             method= {RequestMethod.POST,RequestMethod.GET},
             params={"username"})
     public String hello(){
+        return "success";
+    }
+
+    @RequestMapping("/abc/test/ant")
+    public String testAnt(){
+        return "success";
+    }
+
+    @RequestMapping("/test/rest/{username}/{id}")
+    public String testRest(@PathVariable("id") Integer id,@PathVariable("username") String username){
+        System.out.println("id:"+id+"username: "+username);
         return "success";
     }
 }
