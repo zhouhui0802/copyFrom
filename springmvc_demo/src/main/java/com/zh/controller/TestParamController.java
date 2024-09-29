45587beeaa10d83775801e653f910f9e66b482d2
zhouhui0802 <1741484908@qq.com>
2024-09-29 15:29:17 +0800

    其他简单注解的使用

---
+++ springmvc_demo/src/main/java/com/zh/controller/TestParamController.java
@@ -0,0 +1,32 @@
+package com.zh.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CookieValue;
+import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import javax.servlet.http.HttpServletRequest;
+
+@Controller
+public class TestParamController {
+
+    @RequestMapping("/param/servletAPI")
+    public String getParamByServletAPI(HttpServletRequest request){
+
+        String username=request.getParameter("username");
+        String password=request.getParameter("password");
+        System.out.println("username="+username+": password="+password);
+        return "success";
+    }
+
+    @RequestMapping("/param")
+    public String getParamByServletAPI(@RequestParam(value="username") String username,
+                                       String password,
+                                       @RequestHeader("referer") String referer,
+                                       @CookieValue("JSESSIONID") String jsessionid){
+
+        System.out.println("username:="+username+":"+"password:="+password);
+        return "success";
+    }
+}
