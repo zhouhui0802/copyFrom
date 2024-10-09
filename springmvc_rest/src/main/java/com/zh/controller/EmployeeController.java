95e9aa725a39298ef3aaa7182a5de003ad1be77c
zhouhui0802 <1741484908@qq.com>
2024-10-09 20:07:33 +0800

    完成国庆节之前的留下任务

--- springmvc_rest/src/main/java/com/zh/controller/EmployeeController.java
+++ springmvc_rest/src/main/java/com/zh/controller/EmployeeController.java
@@ -5,6 +5,7 @@ import com.zh.pojo.Employee;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
@@ -21,6 +22,36 @@ public class EmployeeController {
         Collection<Employee> allEmployee=employeeDao.getAll();
         model.addAttribute("allEmployee",allEmployee);
         return "employee_list";
+    }
+
+    @RequestMapping(value = "/employee",method=RequestMethod.POST)
+    public String addEmployee(Employee employee){
+        employeeDao.save(employee);
+
+        return "redirect:/employee";
+    }
+
+    @RequestMapping(value="/employee/{id}",method = RequestMethod.GET)
+    public String toUpdate(@PathVariable("id") Integer id,Model model){
+        Employee employee=employeeDao.get(id);
+
+        model.addAttribute("employee",employee);
+
+        return "employee_update";
+    }
+
+    @RequestMapping(value = "/employee",method = RequestMethod.PUT)
+    public String updateEmployee(Employee employee){
+
+        employeeDao.save(employee);
+
+        return "redirect:/employee";
+    }
+
+    @RequestMapping(value="/employee/{id}",method=RequestMethod.DELETE)
+    public String deleteEmployee(@PathVariable("id") Integer id){
+        employeeDao.delete(id);
+        return "redirect:/employee";
     }
 
 }
