5d0fec03847c52b78f4da88ded57804be3889649
zhouhui0802 <1741484908@qq.com>
2024-10-10 15:20:33 +0800

    文件的上传完成

--- Spring_mvc_ajax/src/main/java/com/zh/controller/FileUpAndDownController.java
+++ Spring_mvc_ajax/src/main/java/com/zh/controller/FileUpAndDownController.java
@@ -6,6 +6,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.util.MultiValueMap;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.multipart.MultipartFile;
 
 import javax.servlet.ServletContext;
 import javax.servlet.http.HttpSession;
@@ -17,6 +18,24 @@ import java.io.InputStream;
 @Controller
 public class FileUpAndDownController {
 
+
+    @RequestMapping("/test/up")
+    public String testUp(MultipartFile photo,HttpSession session) throws IOException{
+
+        String fileName=photo.getOriginalFilename();
+        ServletContext servletContext=session.getServletContext();
+        String photoPath=servletContext.getRealPath("photo");
+        File file=new File(photoPath);
+
+        if(!file.exists()){
+            file.mkdir();
+        }
+
+        String finalPath=photoPath+File.separator+fileName;
+
+        photo.transferTo(new File(finalPath));
+        return "success";
+    }
     @RequestMapping("/test/down")
     public ResponseEntity<byte[]> testResponseEntity(HttpSession session) throws IOException{
         ServletContext servletContext=session.getServletContext();
