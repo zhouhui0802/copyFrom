99b0972d46a8f4e4f12385425bf3d1f0a98e039c
zhouhui0802 <1741484908@qq.com>
2024-10-16 15:11:46 +0800

    细节了解Spring

--- SpringReview01/src/main/java/com/zh/dao/impl/BookDaoImpl.java
+++ SpringReview01/src/main/java/com/zh/dao/impl/BookDaoImpl.java
@@ -4,13 +4,24 @@ import com.zh.dao.BookDao;
 
 public class BookDaoImpl implements BookDao {
 
+    private int connectionNum;
+    private String databaseName;
+
+    public void setConnectionNum(int connectionNum) {
+        this.connectionNum = connectionNum;
+    }
+
+    public void setDatabaseName(String databaseName) {
+        this.databaseName = databaseName;
+    }
+
     public BookDaoImpl() {
         System.out.println("book dao constructor is running...");
     }
 
     @Override
     public void save() {
-        System.out.println("BookDao中save()方法执行");
+        System.out.println("connectionNum="+connectionNum+";databaseName="+databaseName);
     }
 
     public void init(){
